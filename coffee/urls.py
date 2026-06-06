918614689f2ae1e36b8830656d725582e1855ec2
orm_proj_sp <ilybueface@gmail.com>
2026-06-06 19:44:45 +0300

    update my coffee_project

--- coffee/urls.py
+++ coffee/urls.py
@@ -1,11 +1,12 @@
-from coffee.views import drink_list, category, drink_detail, order_list, order_detail
+from coffee.views import DrinkViewSet, OrderViewSet, CategoryViewSet
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 
 
-urlpatterns = [
-    path('drinks/', drink_list),
-    path('category/', category),
-    path('drinks/<int:pk>/', drink_detail),
-    path('orders/', order_list),
-    path('orders/<int:pk>/', order_detail)
-]
+router = DefaultRouter()
+router.register(r'drinks', DrinkViewSet, basename='Drink')
+router.register(r'order', OrderViewSet, basename='Order')
+router.register(r'category', CategoryViewSet, basename='Category')
+
+
+urlpatterns = router.urls
--- coffee/views.py
+++ coffee/views.py
@@ -1,69 +1,32 @@
-from django.contrib.sessions import serializers
-from django.shortcuts import render
-from rest_framework import status
-from rest_framework.generics import get_object_or_404
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from django.db.migrations import serializer
+from rest_framework import viewsets
 from coffee.serilizator import Drinkserializers, Categoryserializers, Orderserializers
 from .models import Drink, Category, Order
-
-@api_view(['GET', 'POST'])
-def drink_list(request):
-    drinks = Drink.objects.all()
-    drk = Drinkserializers(drinks, many=True)
-    if request.method == 'POST':
-        serializer = Drinkserializers(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(drk.data)
+from rest_framework.decorators import api_view, action
+from rest_framework.response import Response
 
 
-@api_view(['GET'])
-def category(request):
-    ctga = Category.objects.all()
-    ctg = Categoryserializers(ctga, many=True)
-    return Response(ctg.data)
+class DrinkViewSet(viewsets.ModelViewSet):
+    queryset = Drink.objects.all()
+    serializer_class = Drinkserializers
 
+    @action(detail=False, methods=['get'])
+    def cheap_drinks(self, request):
+        drinks = Drink.objects.filter(price__lt=300)
+        drks = Drinkserializers(drinks, many=True)
+        return Response(drks.data)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def drink_detail(request, pk):
-    item = get_object_or_404(Drink, id=pk)
-    itemser = Drinkserializers(item)
-    if request.method == 'PUT':
-        serializer = Drinkserializers(item, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-    if request.method == 'DELETE':
-        item.delete()
-        return Response('', status=status.HTTP_204_NO_CONTENT)
-    return Response(itemser.data)
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = Categoryserializers
 
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = Orderserializers
 
-@api_view(['GET', 'POST'])
-def order_list(request):
-    order = Order.objects.all()
-    ord = Orderserializers(order, many=True)
-    if request.method == 'POST':
-        serializer = Orderserializers(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-    return Response(ord.data)
-
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def order_detail(request, pk):
-    order_det = get_object_or_404(Order, id=pk)
-    orddet = Orderserializers(order_det)
-    if request.method == 'PUT':
-        serializers = Orderserializers(order_det, data=request.data)
-        if serializers.is_valid():
-            serializers.save()
-            return Response(serializers.data)
-    if request.method == 'DELETE':
-        order_det.delete()
-        return Response('', status=status.HTTP_204_NO_CONTENT)
-    return Response(order_det.data)
+    @action(detail=True, methods=['GET'])
+    def today_orders(self, request, pk=None):
+        order = self.get_object()
+        serializer = Orderserializers(order)
+        return Response(serializer.data)
 
