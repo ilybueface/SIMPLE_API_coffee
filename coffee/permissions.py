bbd78be65d66d8836deba588d47fd0d99515acad
orm_proj_sp <ilybueface@gmail.com>
2026-06-13 05:29:56 +0300

    update my coffee_project

--- coffee/permissions.py
+++ coffee/permissions.py
@@ -1,6 +1,6 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
-
+re
 class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         if request.method in SAFE_METHODS:
